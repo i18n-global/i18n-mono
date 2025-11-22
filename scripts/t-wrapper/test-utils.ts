80708f1917fd3e7e0d9710798baedf15feed936f
manNomi <hanmw110@naver.com>
2025-11-22 15:35:37 +0900

    refactor: 테스트 파일의 모든 fs 관련 코드를 test-utils로 통합
    - createTempDir, removeDir 유틸리티 함수 추가
    - fs.mkdtempSync, fs.existsSync, fs.rmSync 제거
    - 모든 테스트 파일에서 fs import 제거

--- scripts/t-wrapper/test-utils.ts
+++ scripts/t-wrapper/test-utils.ts
@@ -4,6 +4,7 @@
 
 import * as fs from "fs";
 import * as path from "path";
+import * as os from "os";
 
 /**
  * 파일에 내용을 작성
@@ -17,5 +18,21 @@ export function setFile(filePath: string, content: string): void {
  */
 export function readFile(filePath: string): string {
   return fs.readFileSync(filePath, "utf-8");
+}
+
+/**
+ * 임시 디렉토리 생성
+ */
+export function createTempDir(prefix: string): string {
+  return fs.mkdtempSync(path.join(os.tmpdir(), prefix));
+}
+
+/**
+ * 디렉토리 삭제
+ */
+export function removeDir(dirPath: string): void {
+  if (fs.existsSync(dirPath)) {
+    fs.rmSync(dirPath, { recursive: true, force: true });
+  }
 }
 
