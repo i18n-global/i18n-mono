906ff8d53f9504df18e484bdb6a1848e8ff96659
manNomi <hanmw110@naver.com>
2025-11-26 21:37:16 +0900

    Husky, ESLint 에어비앤비 스타일, 브랜치 보호 설정을 추가하다

--- packages/core/eslint.config.mjs
+++ packages/core/eslint.config.mjs
@@ -2,7 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
@@ -17,4 +26,32 @@ export default [
       },
     },
   },
+  ...compat.extends("airbnb", "airbnb-typescript", "airbnb/hooks"),
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-filename-extension": [
+        1,
+        {
+          extensions: [".tsx", ".ts"]
+        }
+      ],
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          ts: "never",
+          tsx: "never"
+        }
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_"
+        }
+      ],
+      "no-console": "warn",
+      "import/prefer-default-export": "off"
+    }
+  }
 ];
